228ad41d8db2ffd56e6169f8a7ffa0d3074f2d1c
mkwtys <mkwtys@gmail.com>
2018-02-13 20:06:43 +0900

    install:  add .zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-dotfiles=".vim .gitconfig .gitignore_global .npmrc .vimrc .zshrc"
+dotfiles=".vim .zsh .gitconfig .gitignore_global .npmrc .vimrc .zshrc"
 for dotfile in $dotfiles
 do
   ln -s "$PWD/$dotfile" $HOME
