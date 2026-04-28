31613334b6ed2e8fff55a15e496f93e3950cd465
mkwtys <mkwtys@gmail.com>
2026-04-28 11:28:15 +0900

    fix install.sn

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 dotfiles=".vim .zsh .gitconfig .gitignore_global .npmrc .vimrc .zshrc"
+
 for dotfile in $dotfiles
 do
-  ln -s "$PWD/$dotfile" $HOME
+  ln -snfv "$PWD/$dotfile" "$HOME/$dotfile"
 done
